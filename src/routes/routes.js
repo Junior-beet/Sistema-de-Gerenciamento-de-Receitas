db338c746448b494d3da7356db7c2f2dfb57eb5e
AKILA MARIA DA SILVA VICENTE <akila.vicente2@senaisp.edu.br>
2026-05-07 15:59:00 -0300

    Funcionalidade de usuario nada

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import usuarioRoutes from "./usuarioRoutes.js";
-
-const routes = Router();
-
-routes.use('/usuarios', usuarioRoutes);
-
-export default routes;
--- src/routes/usuarioRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import usuarioController from '../controllers/usuarioController.js';
-
-const usuarioRoutes = Router();
-
-usuarioRoutes.post('/', usuarioController.criar);
-usuarioRoutes.get('/', usuarioController.selecionar);
-
-export default usuarioRoutes;
