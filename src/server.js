d57a80acd94054cb9e8dc84ad8e273267c76cc4d
SamuelRab <samuelrabelodasilva@gmail.com>
2026-07-02 23:11:53 -0300

    RF-015: Troca de senha

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/routes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/', routes);
